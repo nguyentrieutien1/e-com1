f34c543462bbf2dd5991640dd34283775e5988d3
Trieu Tien Nguyen <59260191+nguyentrieutien1@users.noreply.github.com>
2023-03-10 07:48:21 +0700

    D

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,10 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import L from "../L";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    // loader: rootLoader,
+    // children: [
+    //   {
+    //     path: "team",
+    //     element: <Team />,
+    //     loader: teamLoader,
+    //   },
+    // ],
+  },
+  {
+    path: "/a",
+    element: <L />,
     // loader: rootLoader,
     // children: [
     //   {
